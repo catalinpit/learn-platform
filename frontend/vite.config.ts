5f125748e4d1e75fe7fae9c0c4409fcba487bd0a
Catalin Pit <25515812+catalinpit@users.noreply.github.com>
2024-12-12 20:36:12 +0200

    feat: add tanstack router

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,12 @@
 import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+
 import path from "node:path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [TanStackRouterVite(), react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
