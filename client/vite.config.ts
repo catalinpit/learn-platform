b8ee47abb57dd76e23f6f8c612df96d530be3feb
Catalin Pit <25515812+catalinpit@users.noreply.github.com>
2025-03-01 16:46:58 +0200

    chore: apply cors to all routes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -30,7 +30,6 @@ export default defineConfig({
       "/api": {
         target: "http://127.0.0.1:9999",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
