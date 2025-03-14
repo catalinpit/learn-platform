f89a4e2262bede6c5cec010f2ab8cf031016b3ef
Catalin Pit <me+github@catalins.tech>
2025-03-14 20:55:35 +0200

    chore: remove baseURL auth client

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -2,7 +2,6 @@ import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseUrl: "http://localhost:9999",
   plugins: [
     inferAdditionalFields({
       user: {
