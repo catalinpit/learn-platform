376e1fd6e1d3d19a3b3e73c4b84c21dc1e8724fa
Catalin Pit <me+github@catalins.tech>
2025-03-15 15:19:54 +0200

    chore: small changes

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -1,6 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
-import { polar } from "@polar-sh/better-auth";
 
 export const authClient = createAuthClient({
   plugins: [
