79a7bd7915d0ef6e5550a0206c5302a570ad72ca
Catalin Pit <me+github@catalins.tech>
2025-04-05 21:20:41 +0300

    style: update CourseHeader description text color and font weight

--- client/src/components/course-header.tsx
+++ client/src/components/course-header.tsx
@@ -7,7 +7,7 @@ export function CourseHeader({ title, description }: CourseHeaderProps) {
   return (
     <div className="flex flex-col gap-1 items-center font-medium pb-6">
       <h2 className="text-4xl md:text-5xl leading-10 pb-2">{title}</h2>
-      <p className="text-neutral-400 dark:text-neutral-500 text-center text-md md:text-lg">
+      <p className="text-neutral-600 dark:text-neutral-400 text-center font-light text-md md:text-lg">
         {description}
       </p>
     </div>
