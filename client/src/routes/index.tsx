fc0f5f76eb76ca84e7a94a4ecbf2c06904185389
catalinpit <catalinpit@gmail.com>
2025-05-24 11:43:27 +0300

    chore: remove auto-deploy text

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -80,7 +80,7 @@ function Index() {
     <div className="p-6">
       <div className="pb-12 flex-1">
         <CourseHeader
-          title="All courses auto-deploy"
+          title="All courses"
           description="Browse all the available courses and learn something new"
         />
         <Input
