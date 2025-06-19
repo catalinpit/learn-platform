65269e90c60ae0e4b3db06ca7e7a7c001682b5e1
catalinpit <catalinpit@gmail.com>
2025-06-19 18:51:39 +0300

    chore: more changes

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,44 +1,31 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { useSession } from "@/lib/auth-client";
+import { QueryClient } from "@tanstack/react-query";
+import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import NotFound from "./components/not-found";
 
 import { routeTree } from "./routeTree.gen";
 import "./index.css";
 import { LoadingSpinner } from "./components/ui/loading-spinner";
 
-const queryClient = new QueryClient();
 
-const router = createRouter({
-  routeTree,
-  context: { auth: undefined, queryClient },
-  defaultPendingComponent: () => {
-    return <LoadingSpinner fullScreen />;
-  },
-  defaultNotFoundComponent: () => {
-    return <NotFound />;
-  },
-});
+export function createRouter() {
+  const queryClient = new QueryClient();
 
-declare module "@tanstack/react-router" {
+  const router = createTanStackRouter({
+    routeTree,
+    context: { auth: undefined, queryClient },
+    defaultPendingComponent: () => {
+      return <LoadingSpinner fullScreen />;
+    },
+    defaultNotFoundComponent: () => {
+      return <NotFound />;
+    },
+  });
+
+  return router;
+}
+
+declare module '@tanstack/react-router' {
   interface Register {
-    router: typeof router;
+    router: ReturnType<typeof createRouter>
   }
 }
-
-export function RouterWithAuthContext() {
-  const { data: auth, isPending, error } = useSession();
-
-  if (!isPending && !error) {
-    return <RouterProvider router={router} context={{ auth }} />;
-  }
-}
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterWithAuthContext />
-    </QueryClientProvider>
-  </StrictMode>
-);
