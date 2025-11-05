b37c325139db132461c6304368293dee6f021816
Catalin Pit <catalinpit@gmail.com>
2025-11-05 19:18:19 +0200

    chore: adjust Prisma client import path for consistency and improve code formatting

--- apps/server/db/index.ts
+++ apps/server/db/index.ts
@@ -1,15 +1,17 @@
 import env from "@/env";
 
-import { PrismaClient } from "../prisma/generated/client";
+import { PrismaClient } from "../prisma/generated/client/client";
 
 declare global {
-  // eslint-disable-next-line no-var, vars-on-top
+  // eslint-disable-next-line vars-on-top
   var prisma: PrismaClient;
 }
 
-const prisma = globalThis.prisma || new PrismaClient({
-  log: ["query", "error", "warn"],
-});
+const prisma =
+  globalThis.prisma ||
+  new PrismaClient({
+    log: ["query", "error", "warn"],
+  });
 
 if (env.NODE_ENV !== "production") {
   globalThis.prisma = prisma;
